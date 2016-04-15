458a185cb21f54e6e8d7714533d3c72d11c515af
Martin Contento <tinnet@users.noreply.github.com>
2016-04-15 09:50:36 +0200

    feat: Do not increment runTime when idle (#7)
    feat: Do not increment runTime when idle

--- desktime/StatusMenuController.swift
+++ desktime/StatusMenuController.swift
@@ -7,15 +7,36 @@
 //
 
 import Cocoa
+import Foundation
 
 class StatusMenuController: NSObject {
     @IBOutlet weak var statusMenu: NSMenu!
 
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
     var updateTimer:NSTimer?
-    var lastTick:NSTimeInterval = NSDate().timeIntervalSince1970;
-    var runTime:Double = 0;
-    var showDots:Bool = true;
+    var lastTick:NSTimeInterval = NSDate().timeIntervalSince1970
+    var runTime:Double = 0
+    var showDots:Bool = true
+    var screenIsLocked:Bool = false
+    var screensaverIsRunning:Bool = false;
+
+    let eventMap: [String: Selector] = [
+        "com.apple.screenIsLocked": #selector(onScreenLocked),
+        "com.apple.screenIsUnlocked": #selector(onScreenUnlocked),
+        "com.apple.screensaver.didstart": #selector(onScreenSaverStart),
+        "com.apple.screensaver.didstop": #selector(onScreenSaverStop),
+    ]
+
+    func registerNotifications() {
+        let notificationCenter = NSDistributedNotificationCenter.defaultCenter()
+        for (event, selector) in  eventMap {
+            notificationCenter.addObserver(self, selector: selector, name: event, object: nil)
+        }
+    }
+
+    deinit {
+        NSDistributedNotificationCenter.defaultCenter().removeObserver(self)
+    }
 
     override func awakeFromNib() {
         statusItem.title = "00:00"
@@ -33,13 +54,38 @@ class StatusMenuController: NSObject {
 //        let icon = NSImage(named: "StatusIcon")
 //        icon?.template = true
 //        statusItem.image = icon
+
+        registerNotifications()
+    }
+
+    func isIdle() -> Bool {
+        return screensaverIsRunning || screenIsLocked
+    }
+
+    func onScreenLocked() {
+        screenIsLocked = true
+    }
+
+    func onScreenUnlocked() {
+        screenIsLocked = false
+    }
+
+    func onScreenSaverStart() {
+        screensaverIsRunning = true
+    }
+
+    func onScreenSaverStop() {
+        screensaverIsRunning = false
     }
 
     func onTimerTick() {
         // Calculate time delta
         let now = NSDate().timeIntervalSince1970;
         let diff = now - lastTick;
-        runTime += diff;
+
+        if !isIdle() {
+            runTime += diff;
+        }
 
         // Update attributes
         lastTick = now;
