9b528e13139c6a3010553b0b1c8496544ff1e988
Michael Contento <michaelcontento@gmail.com>
2016-04-26 09:57:04 +0200

    store runtime in NSDefaults (closes #1)

--- desktime/Timer.swift
+++ desktime/Timer.swift
@@ -13,11 +13,34 @@ class Timer: NSObject {
     var _lastDayOfYear:Int = -1;
     var _runTime:Double = 0;
     var _active:Bool = true;
+    let _prefs = NSUserDefaults.standardUserDefaults()
 
     func _getDayOfYear() -> Int {
         let date = NSDate()
         let cal = NSCalendar.currentCalendar()
         return cal.ordinalityOfUnit(.Day, inUnit: .Year, forDate: date)
+    }
+
+    func _getDateString() -> String {
+        let date = NSDate();
+        let dateFormatter = NSDateFormatter();
+        dateFormatter.dateFormat = "yyyy-MM-dd";
+        return dateFormatter.stringFromDate(date);
+    }
+
+    override init() {
+        super.init();
+
+        _lastDayOfYear = _getDayOfYear();
+        _loadTime();
+    }
+
+    func _loadTime() {
+        _runTime = _prefs.doubleForKey(_getDateString());
+    }
+
+    func _saveTime() {
+        _prefs.setDouble(_runTime, forKey: _getDateString());
     }
 
     func enable(flag:Bool? = false) {
@@ -51,6 +74,8 @@ class Timer: NSObject {
         if (currentDayOfYear != _lastDayOfYear) {
             _lastDayOfYear = currentDayOfYear;
             _runTime = 0;
+        } else {
+            _saveTime();
         }
 
         return _runTime;
